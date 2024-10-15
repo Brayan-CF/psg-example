d8439e1c2aaa70b1f449f19aeb469231832e66dd
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2024-10-15 13:49:04 -0400

    tutorial fast

---
+++ Ejercicios/ejercicio_03.py
@@ -0,0 +1,5 @@
+print("para barrer un piso necesitamos una escaba, basurero y un poco de agua para no levantar tanto volvo")
+print("con eso procedemos a hechar un poco de gua al piso salpicando y barremos")
+print("luego d montanar la basura usamos el basurero y votamos la basura")
+for i in range(3):
+    print("shu shu")
--- ejercicio_02.py
+++
@@ -1,6 +0,0 @@
-print("Lechuga: Como esta!!")
-print("Tomate: muy fresco y vos ")
-print("Lechuga: super molesto")
-
-for i in range(4):
-    print("tomate: por que :O!!!")
