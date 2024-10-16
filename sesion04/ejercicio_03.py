7c1bb90e3d3c9296bbb1ad7e22421e660c173337
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2024-10-15 20:20:35 -0400

    historia

---
+++ sesion04/ejercicio_03.py
@@ -0,0 +1,9 @@
+# historia de juego
+print("CANICAS")
+print("1. ponemos un par de canicas en el suelo")
+print("2. nos alejamos un distancia considerable")
+print("3. desde un lugar ya fijado procedemos a arojar otra  canica")
+print("4. y la canica que este mas cerca, esa sera la que inicie el juego")
+print("5. con las manos apuntamos y disparamos")
+print("6. objetivo, hacer chocar esas canicas del suelo con que nosotros lanzamos")
+
