1913856687f2ac35ea66434ea14d487dfd663cbd
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2024-10-19 22:12:41 -0400

    sesioin 07

--- sesion07/sesion07.py
+++ sesion07/sesion07.py
@@ -265,8 +265,9 @@ formateado = cadena.format(pais=pais, ciudad=ciudad)
 print (cadena)
 print (formateado) '''
 # formaterar con s-strings
+'''
 print ("Función format con f-string")
 moneda = "Boliviano"
 pais = "Bolivia"
 formateado = f"La moneda de {pais} es el {moneda}"
-print (formateado)
+print (formateado)'''
