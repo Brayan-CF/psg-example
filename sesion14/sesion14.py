1047658eb75d1322d7c0e73ce1d8d652efa1145c
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2025-01-03 16:27:43 -0400

    iniciando sesion14

---
+++ sesion14/sesion14.py
@@ -0,0 +1,32 @@
+# Sesion 14
+# funciones
+# Bloques de codigo
+'''-----------------------------------------------------'''
+# Declaracion de una Funcion:
+'''
+def nombre_funcion():
+    print ("Bloque de código")
+'''
+'''
+
+    def es la palabra reservada
+    nombre_funcion es el nombre de la función
+    () son los paréntesis que pueden contener parámetros
+    : es el delimitador de inicio del bloque de código
+    print es el código a ejecutar
+    Posee un nivel de indentación
+
+'''
+# para devolver un valor: return
+'''
+def nombre_funcion():
+    return "Bloque de código"
+'''
+# para llamar a la funcion:
+'''
+def nombre_funcion():
+    print ("Bloque de código")
+
+nombre_funcion()
+print (type(nombre_funcion))
+'''
