c0b5f8d3bbd9ba4622ab83849e2efa5722dfb244
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2025-01-06 21:08:45 -0400

    propuestas de retos sesion15

---
+++ sesion15/retos_sesion_15/ejercicio_01.py
@@ -0,0 +1,6 @@
+'''
+Crear una calculadora que solicite dos números y realice las 
+operaciones básicas de suma, resta, multiplicación y división con 
+manejo de excepciones, para salir del programa se debe ingresar 
+"salir"
+'''
